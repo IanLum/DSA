ecdb62985ad86873bc2cc6197a6da6aa191eb968
Ian Lum <ianlum314@gmail.com>
2024-02-01 14:37:52 -0500

    tweak number of elements to test

--- InClass_5_MutableList/src/Main.kt
+++ InClass_5_MutableList/src/Main.kt
@@ -1,7 +1,7 @@
 import kotlin.time.measureTime
 
 fun main() {
-    val arraySizes = listOf(100, 1000, 10000, 100000, 1000000, 10000000, 100000000)
+    val arraySizes = listOf(100, 1000, 10000, 100000) //, 1000000, 10000000, 100000000)
     println("numberOfElements totalTime timePerElement")
     for (arraySize in arraySizes) {
         val myList = MyMutableIntList()
