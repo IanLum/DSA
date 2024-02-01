4069df8d85748594d523060f6c13707eaad444ab
Ian Lum <ianlum314@gmail.com>
2024-02-01 11:21:22 -0500

    in class 5

---
+++ InClass_5_MutableList/src/MyMutableIntList.kt
@@ -0,0 +1,47 @@
+class MyMutableIntList {
+    private var array = IntArray(1)
+    private var s = 0
+
+    /**
+     * Add [element] to the end of the list
+     */
+    fun add(element: Int) {
+        if (s == array.size) {
+            array = array.copyOf(array.size * 2)
+        }
+        array[s] = element
+        s++
+    }
+
+    /**
+     * Remove all elements from the list
+     */
+    fun clear() {
+        array = IntArray(array.size)
+        s = 0
+    }
+
+    /*
+     * @return the size of the list
+     */
+    fun size(): Int {
+        return s
+    }
+
+    /**
+     * @param index the index to return
+     * @return the element at [index]
+     */
+    operator fun get(index: Int):Int {
+        return array[index]
+    }
+
+    /**
+     * Store [value] at position [index]
+     * @param index the index to set
+     * @param value to store at [index]
+     */
+    operator fun set(index: Int, value: Int) {
+        array[index] = value
+    }
+}
