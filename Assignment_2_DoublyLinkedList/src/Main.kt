6311d282a63e62c814b4ab0704adeef805bf4673
Ian Lum <ianlum314@gmail.com>
2024-02-04 11:06:39 -0500

    implement copystack

--- Assignment_2_DoublyLinkedList/src/Main.kt
+++ Assignment_2_DoublyLinkedList/src/Main.kt
@@ -1,3 +1,28 @@
+// P5: Copy stack problem
+
+fun <T> copyStack(og: MyStack<T>): MyStack<T> {
+    val q = MyQueue<T>()
+    val copy = MyStack<T>()
+    while (!og.isEmpty()) {
+        copy.push(og.pop()!!)
+    }
+    while (!copy.isEmpty()) {
+        q.enqueue(copy.pop()!!)
+    }
+    while (!q.isEmpty()) {
+        val x: T = q.dequeue()!!
+        og.push(x)
+        copy.push(x)
+    }
+    return copy
+}
 fun main() {
-    println("Hello World!")
+    val original = MyStack<Int>()
+    original.push(0)
+    original.push(1)
+    original.push(2)
+    original.push(3)
+    val copy = copyStack(original)
+    println("${copy.pop()}, ${copy.pop()}, ${copy.pop()}, ${copy.pop()}")
+    println("${original.pop()}, ${original.pop()}, ${original.pop()}, ${original.pop()}")
 }
