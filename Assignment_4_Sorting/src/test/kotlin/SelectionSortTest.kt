7d37240d7a9d2a9cd6885104b4b8c5c95e83a4d6
Ian Lum <ianlum314@gmail.com>
2024-02-21 16:36:28 -0500

    selection sort test

---
+++ Assignment_4_Sorting/src/test/kotlin/SelectionSortTest.kt
@@ -0,0 +1,34 @@
+import org.sorting.selectionSort
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class SelectionSortTest {
+
+    @Test
+    fun singleDigit() {
+        val l = mutableListOf(4,8,2,3,8,5)
+        val exp = mutableListOf(2,3,4,5,8,8)
+        assertEquals(exp, selectionSort(l))
+    }
+    @Test
+    fun multiDigit() {
+        val l = mutableListOf(4345,6896,347,568,3,36,209)
+        val exp = mutableListOf(3,36,209,347,568,4345,6896)
+        assertEquals(exp, selectionSort(l))
+    }
+
+    @Test
+    fun allSame() {
+        val l = mutableListOf(3,3,3,3)
+        val exp = mutableListOf(3,3,3,3)
+        assertEquals(exp, selectionSort(l))
+    }
+
+    @Test
+    fun duplicates() {
+        val l = mutableListOf(3,4,3,4,3,4,3)
+        val exp = mutableListOf(3,3,3,3,4,4,4)
+        assertEquals(exp, selectionSort(l))
+    }
+}
