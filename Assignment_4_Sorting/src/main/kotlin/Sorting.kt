fcdc9c49dc0a24efbdcde2bd6772eead5918bd20
Ian Lum <ianlum314@gmail.com>
2024-02-21 11:28:10 -0500

    declare sorting algs

---
+++ Assignment_4_Sorting/src/main/kotlin/Sorting.kt
@@ -0,0 +1,17 @@
+package org.example
+
+fun selectionSort(data: Array<Int>): Array<Int> {
+    TODO()
+}
+
+fun quickSort(data: Array<Int>): Array<Int> {
+    TODO()
+}
+
+fun mergeSort(data: Array<Int>): Array<Int> {
+    TODO()
+}
+
+fun radixSort(data: Array<Int>): Array<Int> {
+    TODO()
+}
