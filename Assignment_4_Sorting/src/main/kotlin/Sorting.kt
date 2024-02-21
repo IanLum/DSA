b9e0bf1e5ab8b2b1dd0cde706e0f1e33e97dbb97
Ian Lum <ianlum314@gmail.com>
2024-02-21 13:26:17 -0500

    impl quicksort, use list instead of mutlist

--- Assignment_4_Sorting/src/main/kotlin/Sorting.kt
+++ Assignment_4_Sorting/src/main/kotlin/Sorting.kt
@@ -2,8 +2,8 @@ package org.example
 
 import java.util.*
 
-fun selectionSort(data: MutableList<Int>): MutableList<Int> {
-    for (startIdx in 0..<data.size) {
+fun selectionSort(data: List<Int>): List<Int> {
+    for (startIdx in data.indices) {
         var min = Pair(startIdx, data[startIdx])
         for ((idx, num) in data.drop(startIdx).withIndex())
             if (num < min.second) min = Pair(idx+startIdx, num)
@@ -12,8 +12,21 @@ fun selectionSort(data: MutableList<Int>): MutableList<Int> {
     return data
 }
 
-fun quickSort(data: MutableList<Int>): MutableList<Int> {
-    TODO()
+fun quickSort(data: List<Int>): List<Int> {
+    if (data.size <= 1) return data
+
+    val pivot = data[0]
+    var storeIdx = 1
+    for ((idx, num) in data.drop(1).withIndex()) {
+        if (num < pivot) {
+            Collections.swap(data, storeIdx, idx + 1)
+            ++storeIdx
+        }
+    }
+    val headPartition = data.slice(1..<storeIdx)
+    val tailPartition = data.drop(storeIdx)
+
+    return quickSort(headPartition) + data[0] + quickSort(tailPartition)
 }
 
 fun mergeSort(data: MutableList<Int>): MutableList<Int> {
