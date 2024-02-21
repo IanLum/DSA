77e3eb0e2b0a2b1bc69f07c3c9ba3d0a68f93ff5
Ian Lum <ianlum314@gmail.com>
2024-02-21 16:40:33 -0500

    quick sort tests

---
+++ Assignment_4_Sorting/src/test/kotlin/QuickSortTest.kt
@@ -0,0 +1,42 @@
+import org.sorting.quickSort
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.sorting.selectionSort
+
+class QuickSortTest {
+
+    @Test
+    fun singleDigit() {
+        val l = mutableListOf(4,8,2,3,8,5)
+        val exp = mutableListOf(2,3,4,5,8,8)
+        assertEquals(exp, quickSort(l))
+    }
+    @Test
+    fun multiDigit() {
+        val l = mutableListOf(4345,6896,347,568,3,36,209)
+        val exp = mutableListOf(3,36,209,347,568,4345,6896)
+        assertEquals(exp, quickSort(l))
+    }
+
+    @Test
+    fun allSame() {
+        val l = mutableListOf(3,3,3,3)
+        val exp = mutableListOf(3,3,3,3)
+        assertEquals(exp, quickSort(l))
+    }
+
+    @Test
+    fun duplicates() {
+        val l = mutableListOf(3,4,3,4,3,4,3)
+        val exp = mutableListOf(3,3,3,3,4,4,4)
+        assertEquals(exp, quickSort(l))
+    }
+
+    @Test
+    fun empty() {
+        val l = mutableListOf<Int>()
+        val exp = mutableListOf<Int>()
+        assertEquals(exp, quickSort(l))
+    }
+}
