889d3bbddd3644d0bb87d8f35143b1aba1e86783
Ian Lum <ianlum314@gmail.com>
2024-02-21 11:51:38 -0500

    implement selection sort

--- Assignment_4_Sorting/src/main/kotlin/Sorting.kt
+++ Assignment_4_Sorting/src/main/kotlin/Sorting.kt
@@ -1,7 +1,15 @@
 package org.example
 
+import java.util.*
+
 fun selectionSort(data: MutableList<Int>): MutableList<Int> {
-    TODO()
+    for (startIdx in 0..<data.size) {
+        var min = Pair(startIdx, data[startIdx])
+        for ((idx, num) in data.drop(startIdx).withIndex())
+            if (num < min.second) min = Pair(idx+startIdx, num)
+        Collections.swap(data, startIdx, min.first)
+    }
+    return data
 }
 
 fun quickSort(data: MutableList<Int>): MutableList<Int> {
