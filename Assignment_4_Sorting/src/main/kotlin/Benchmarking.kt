5514f5553ace9f4944206e0455523ab4adc67e5c
Ian Lum <ianlum314@gmail.com>
2024-02-21 21:58:21 -0500

    print for google sheets

--- Assignment_4_Sorting/src/main/kotlin/Benchmarking.kt
+++ Assignment_4_Sorting/src/main/kotlin/Benchmarking.kt
@@ -1,9 +1,10 @@
 package org.sorting
 
 import kotlin.random.Random
+import kotlin.time.DurationUnit
 import kotlin.time.measureTime
 
-fun benchmarking(minVal: Int, maxVal: Int, listSizes: List<Int>) {
+fun benchmarking(minVal: Int, maxVal: Int, listSizes: List<Int>, forGSheets: Boolean = false) {
     val randomList = List(listSizes.max()) { Random.nextInt(minVal, maxVal)}
     println("List Size | Selection | Merge | Quick | Radix")
     listSizes.forEach {
@@ -13,9 +14,16 @@ fun benchmarking(minVal: Int, maxVal: Int, listSizes: List<Int>) {
         val quickTime = measureTime { quickSort(list) }
         val radixTime = measureTime { radixSort(list) }
         print(it)
-        print(" | $selectionTime")
-        print(" | $mergeTime")
-        print(" | $quickTime")
-        print(" | $radixTime\n")
+        if (forGSheets) {
+            print("\t${selectionTime.toDouble(DurationUnit.MILLISECONDS)}")
+            print("\t${mergeTime.toDouble(DurationUnit.MILLISECONDS)}")
+            print("\t${quickTime.toDouble(DurationUnit.MILLISECONDS)}")
+            print("\t${radixTime.toDouble(DurationUnit.MILLISECONDS)}\n")
+        } else {
+            print("| $selectionTime")
+            print("| $mergeTime")
+            print("| $quickTime")
+            print("| $radixTime\n")
+        }
     }
 }
--- Assignment_4_Sorting/src/main/kotlin/Main.kt
+++ Assignment_4_Sorting/src/main/kotlin/Main.kt
@@ -4,10 +4,11 @@ import kotlin.math.pow
 
 fun main() {
     val sizes = listOf(100, 500, 1000, 5000, 9000)
+    val b = true
     println("Values 0-100")
-    benchmarking(0, 100, sizes)
+    benchmarking(0, 100, sizes, b)
     println("Values 0-10^9")
-    benchmarking(0, 10.0.pow(9).toInt(), sizes)
+    benchmarking(0, 10.0.pow(9).toInt(), sizes, b)
     println("Values 10^8-10^9")
-    benchmarking(10.0.pow(8).toInt(), 10.0.pow(9).toInt(), sizes)
+    benchmarking(10.0.pow(8).toInt(), 10.0.pow(9).toInt(), sizes, b)
 }
