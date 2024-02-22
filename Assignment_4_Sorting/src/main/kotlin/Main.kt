4bf623c631e25691f42f9e07e20d5a6a385d4ed0
Ian Lum <ianlum314@gmail.com>
2024-02-21 20:17:54 -0500

    impl benchmarking

--- Assignment_4_Sorting/src/main/kotlin/Main.kt
+++ Assignment_4_Sorting/src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
-package org.example
+package org.sorting
+
+import kotlin.math.pow
 
 fun main() {
-    println("Hello World!")
+    val sizes = listOf(100, 500, 1000, 5000, 9000)
+    println("Values 0-100")
+    benchmarking(0, 100, sizes)
+    println("Values 0-10^9")
+    benchmarking(0, 10.0.pow(9).toInt(), sizes)
+    println("Values 10^8-10^9")
+    benchmarking(10.0.pow(8).toInt(), 10.0.pow(9).toInt(), sizes)
 }
