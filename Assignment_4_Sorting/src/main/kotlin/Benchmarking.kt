575d64948d17b053f8500f532e5cfe350c7d65cd
Ian Lum <ianlum314@gmail.com>
2024-02-21 20:38:52 -0500

    change printing

--- Assignment_4_Sorting/src/main/kotlin/Benchmarking.kt
+++ Assignment_4_Sorting/src/main/kotlin/Benchmarking.kt
@@ -3,8 +3,9 @@ package org.sorting
 import kotlin.random.Random
 import kotlin.time.measureTime
 
-    fun benchmarking(minVal: Int, maxVal: Int, listSizes: List<Int>) {
+fun benchmarking(minVal: Int, maxVal: Int, listSizes: List<Int>) {
     val randomList = List(listSizes.max()) { Random.nextInt(minVal, maxVal)}
+    println("List Size | Selection | Merge | Quick | Radix")
     listSizes.forEach {
         val list = randomList.take(it)
         val selectionTime = measureTime { selectionSort(list) }
@@ -12,9 +13,9 @@ import kotlin.time.measureTime
         val quickTime = measureTime { quickSort(list) }
         val radixTime = measureTime { radixSort(list) }
         print(it)
-        print(" | Selection: $selectionTime")
-        print(" | Merge: $mergeTime")
-        print(" | Quick: $quickTime")
-        print(" | Radix: $radixTime\n")
+        print(" | $selectionTime")
+        print(" | $mergeTime")
+        print(" | $quickTime")
+        print(" | $radixTime\n")
     }
 }
