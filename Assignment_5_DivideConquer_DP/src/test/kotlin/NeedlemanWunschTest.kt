66c4d3775b8352d157823dac9939c146950714ce
Ian Lum <ianlum314@gmail.com>
2024-02-27 20:54:45 -0500

    write tests for needleman wunsch

--- Assignment_5_DivideConquer_DP/src/test/kotlin/NeedlemanWunschTest.kt
+++ Assignment_5_DivideConquer_DP/src/test/kotlin/NeedlemanWunschTest.kt
@@ -1,3 +1,4 @@
+import org.example.needlemanWunsch
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
@@ -5,6 +6,26 @@ import org.junit.jupiter.api.Assertions.*
 class NeedlemanWunschTest {
 
     @Test
-    fun needlemanWunsch() {
+    fun diffLengthStrings() {
+        assertEquals(
+            Pair("AATCG", "--TCG"),
+            needlemanWunsch("AATCG", "TCG")
+        )
+    }
+
+    @Test
+    fun bothGap() {
+        assertEquals(
+            Pair("AT-CG", "-TGCG"),
+            needlemanWunsch("ATCG", "TGCG")
+        )
+    }
+
+    @Test
+    fun branchingAnswer() {
+        assertEquals(
+            Pair("G-ATTACA", "GCAT-GCG"),
+            needlemanWunsch("GATTACA", "GCATGCG")
+        )
     }
 }
