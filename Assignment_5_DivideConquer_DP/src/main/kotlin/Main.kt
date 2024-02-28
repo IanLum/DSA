9852c519b137b847d0b83250caf621dabd661db4
Ian Lum <ianlum314@gmail.com>
2024-02-27 20:56:59 -0500

    configure main

--- Assignment_5_DivideConquer_DP/src/main/kotlin/Main.kt
+++ Assignment_5_DivideConquer_DP/src/main/kotlin/Main.kt
@@ -1,5 +1,19 @@
 package org.example
 
+import kotlin.math.log2
+import kotlin.math.pow
+
 fun main() {
-    println("Hello World!")
+    println("---- Matrix Multiplication Benchmarking ----")
+    val sizes = listOf(2,5,10,20,50,100,200,500)
+    benchmarking(0, 20, sizes, false)
+
+    println("---- Needleman Wunsch Alignment ----")
+    val q = needlemanWunsch(
+        "GATTACA",
+        "GCATGCG",
+        true
+    )
+    println(q.first)
+    println(q.second)
 }
