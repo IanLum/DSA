250054bfce03f80201fce5f787711a7ad621f6f6
Ian Lum <ianlum314@gmail.com>
2024-02-27 19:38:09 -0500

    add backtrace for nw alg

--- Assignment_5_DivideConquer_DP/src/main/kotlin/NeedlemanWunsch.kt
+++ Assignment_5_DivideConquer_DP/src/main/kotlin/NeedlemanWunsch.kt
@@ -8,7 +8,7 @@ var sequence1: String = "" // rows
 var sequence2: String = "" // columns
 var table: Array<IntArray> = emptyArray()
 
-fun needlemanWunsch(seq1: String, seq2: String): Pair<String, String> {
+fun needlemanWunsch(seq1: String, seq2: String, showTable: Boolean = false): Pair<String, String> {
     sequence1 = " $seq1"
     sequence2 = " $seq2"
     val rows = sequence1.length
@@ -16,11 +16,12 @@ fun needlemanWunsch(seq1: String, seq2: String): Pair<String, String> {
     table = Array(rows) { IntArray(cols) { Int.MIN_VALUE } }
     fillTable(rows-1, cols-1)
 
-    table.forEach {
-        println(it.toList())
-    }
+    if (showTable)
+        table.forEach {
+            println(it.toList())
+        }
 
-    return Pair("","")
+    return backtrace(rows-1, cols-1, Pair("",""))
 }
 
 fun fillTable(row: Int, col: Int): Int {
@@ -45,4 +46,40 @@ fun fillTable(row: Int, col: Int): Int {
         )
     }
     return table[row][col]
+}
+
+fun backtrace(row: Int, col: Int, strings: Pair<String, String>): Pair<String, String> {
+    fun traceDiag(row: Int, col: Int, strings: Pair<String, String>): Pair<String, String> =
+        backtrace(
+            row-1, col-1,
+            Pair(sequence1[row] + strings.first, sequence2[col] + strings.second)
+        )
+    fun traceLeft(row: Int, col: Int, strings: Pair<String, String>): Pair<String, String> =
+        backtrace(
+            row, col-1,
+            Pair("-" + strings.first, sequence2[col] + strings.second)
+        )
+    fun traceUp(row: Int, col: Int, strings: Pair<String, String>): Pair<String, String> =
+        backtrace(
+            row-1, col,
+            Pair(sequence1[row] + strings.first, "-" + strings.second)
+        )
+
+    if ((row == 0) and (col == 0))
+        return strings
+    if (row == 0)
+        return traceLeft(row, col, strings)
+    if (col == 0)
+        return traceUp(row, col, strings)
+
+    return when (maxOf(
+        table[row-1][col-1],
+        table[row][col-1],
+        table[row-1][col],
+    )) {
+        table[row-1][col-1] -> traceDiag(row, col, strings)
+        table[row][col-1] -> traceLeft(row, col, strings)
+        table[row-1][col] -> traceUp(row, col, strings)
+        else -> Pair("", "")
+    }
 }
