942b9eaf12597301c26014275afbabab61a99dea
Ian Lum <ianlum314@gmail.com>
2024-03-08 18:30:12 -0500

    refactor examples into separate file

--- DeepDive_1/src/main/kotlin/Main.kt
+++ DeepDive_1/src/main/kotlin/Main.kt
@@ -1,62 +1,6 @@
 package org.example
-import org.jetbrains.kotlinx.kandy.dsl.plot
-import org.jetbrains.kotlinx.kandy.letsplot.export.save
-import org.jetbrains.kotlinx.kandy.letsplot.feature.layout
-import org.jetbrains.kotlinx.kandy.letsplot.layers.line
-import org.jetbrains.kotlinx.kandy.letsplot.layers.points
-import kotlin.math.*
-import kotlin.random.Random
 
-fun simpleFFT() {
-    val t = List(16) { it }
-    val x = t.map { Complex(sin(it.toDouble()), 0.0) }
-    val y = fft(x)
-    val yAbs = y.map { it.abs() }
-    println(yAbs)
-
-    plot() {
-        points {
-            x(t)
-            y(yAbs)
-        }
-    }.save("fft.png")
-}
-
-// https://www.mathworks.com/help/matlab/ref/fft.html
-fun matlabExample() {
-    val Fs = 1000.0 // Sampling frequency
-    val T = 1.0/Fs // Sampling period
-    val L = 1024 // Length of signal
-    val t = List(L) {it * T} // Time vector
-
-    val X = t.map {
-        0.8 + 0.7 * sin(2*PI*50*it) + sin(2*PI*120*it) + // Signal
-                Random.nextDouble(-1.0, 1.0) // Add Noise
-    }
-
-    plot() {
-        line {x(t); y(X)}
-        layout {
-            title = "Noisy Signal"
-            xAxisLabel = "t (s)"
-            yAxisLabel = "X(t)"
-        }
-    }.save("noisy_signal.png")
-
-    val Y = fft(X.map { Complex(it, 0.0) })
-
-    val YAbs = Y.map { it.abs() }
-    val freq = List(L) { Fs / L * it }
-
-    plot() {
-        line { x(freq); y(YAbs) }
-        layout {
-            title = "FFT of Noisy Signal"
-            xAxisLabel = "frequency (Hz)"
-            yAxisLabel = "Amplitude"
-        }
-    }.save("noisy_signal_fft.png")
-}
 fun main() {
-    matlabExample()
+    FourierExample.matlab()
+    FourierExample.simple()
 }
