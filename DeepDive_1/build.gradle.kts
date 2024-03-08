84ea5845051016a3cd0dafa5f61c4bfa7ccc3d2d
Ian Lum <ianlum314@gmail.com>
2024-03-08 17:48:15 -0500

    add kandy (plotting library) to build

--- DeepDive_1/build.gradle.kts
+++ DeepDive_1/build.gradle.kts
@@ -11,8 +11,9 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    implementation("org.jetbrains.kotlinx:multik-core:0.2.3")
-    implementation("org.jetbrains.kotlinx:multik-default:0.2.3")
+    implementation("org.slf4j:slf4j-api:1.6.1")
+    implementation("org.slf4j:slf4j-simple:1.6.1")
+    implementation("org.jetbrains.kotlinx:kandy-lets-plot:0.5.0")
 }
 
 tasks.test {
