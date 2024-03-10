74cc2e1cb35e6cca9684cb9bd3a0e50b84248f82
Ian Lum <ianlum314@gmail.com>
2024-03-09 20:04:42 -0500

    add benchmarking documentation

--- DeepDive_1/src/main/kotlin/Benchmarking.kt
+++ DeepDive_1/src/main/kotlin/Benchmarking.kt
@@ -4,7 +4,10 @@ import kotlin.math.sin
 import kotlin.time.DurationUnit
 import kotlin.time.measureTime
 
-
+/**
+ * Measure and print runtime of [fft] on different length vectors
+ * @param vecLengths: The length of vectors to test [fft] on
+ */
 fun benchmarking(vecLengths: List<Int>) {
     println("Length | Time (s)")
     vecLengths.forEach { length ->
