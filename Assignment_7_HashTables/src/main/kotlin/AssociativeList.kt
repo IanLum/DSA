2cd04968ee0ce05d164aeacd35e7a7b9e727d540
Ian Lum <ianlum314@gmail.com>
2024-03-25 22:05:08 -0400

    impl remove

--- Assignment_7_HashTables/src/main/kotlin/AssociativeList.kt
+++ Assignment_7_HashTables/src/main/kotlin/AssociativeList.kt
@@ -1,7 +1,7 @@
 package org.example
 
 class AssociativeList<K, V>: AssociativeArray<K, V> {
-    class ListNode<K, V>(val key: K, val value: V, val next: ListNode<K, V>?)
+    class ListNode<K, V>(val key: K, val value: V, var next: ListNode<K, V>?)
     private var head: ListNode<K, V>? = null
 
     override fun set(k: K, v: V) {
@@ -23,7 +23,28 @@ class AssociativeList<K, V>: AssociativeArray<K, V> {
     }
 
     override fun remove(k: K): Boolean {
-        TODO("Not yet implemented")
+        head?.also {
+            // Run if head is not null
+            if (it.key == k) {
+                // Special case if head is holding the key to remove
+                head = it.next
+                return true
+            }
+
+            // Otherwise, loop through nodes until the key is found
+            var prev = it
+            var curr = it.next
+            while (curr != null) {
+                if (curr.key == k) {
+                    // Link nodes across the removed value
+                    prev.next = curr.next
+                    return true
+                }
+                prev = curr
+                curr = curr.next
+            }
+        }
+        return false
     }
 
     override fun size(): Int {
