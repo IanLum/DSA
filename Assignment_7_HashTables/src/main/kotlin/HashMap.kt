b9d921a24f97aca5b831788cd7819484e4a67e13
Ian Lum <ianlum314@gmail.com>
2024-03-26 18:59:43 -0400

    init hashmap

---
+++ Assignment_7_HashTables/src/main/kotlin/HashMap.kt
@@ -0,0 +1,27 @@
+package org.example
+
+class HashMap<K, V>: AssociativeArray<K, V> {
+    override fun set(k: K, v: V) {
+        TODO("Not yet implemented")
+    }
+
+    override fun contains(k: K): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun get(k: K): V? {
+        TODO("Not yet implemented")
+    }
+
+    override fun remove(k: K): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun size(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun keyValuePairs(): List<Pair<K, V>> {
+        TODO("Not yet implemented")
+    }
+}
