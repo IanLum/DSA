b575780977a57555f1428d37b8567f385ba98a9c
Ian Lum <ianlum314@gmail.com>
2024-03-26 19:21:08 -0400

    add attributes and goodprimes array

--- Assignment_7_HashTables/src/main/kotlin/HashMap.kt
+++ Assignment_7_HashTables/src/main/kotlin/HashMap.kt
@@ -1,6 +1,40 @@
 package org.example
 
+// https://planetmath.org/goodhashtableprimes
+val goodPrimes = arrayOf(
+    53,
+    97,
+    193,
+    389,
+    769,
+    1543,
+    3079,
+    6151,
+    12289,
+    24593,
+    49157,
+    98317,
+    196613,
+    393241,
+    786433,
+    1572869,
+    3145739,
+    6291469,
+    12582917,
+    25165843,
+    50331653,
+    100663319,
+    201326611,
+    402653189,
+    805306457,
+    1610612741
+)
 class HashMap<K, V>: AssociativeArray<K, V> {
+    private var size = 0
+    private var sizeIdx = 0
+    private var maxSize = goodPrimes[sizeIdx]
+    private val buckets = Array<AssociativeList<K, V>>(maxSize) { AssociativeList() }
+
     override fun set(k: K, v: V) {
         TODO("Not yet implemented")
     }
