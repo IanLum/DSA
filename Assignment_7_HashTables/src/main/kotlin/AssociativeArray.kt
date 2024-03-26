dc72c08d7821a735d951fb9fe5daf6862434fe1f
Ian Lum <ianlum314@gmail.com>
2024-03-25 21:20:40 -0400

    change asso array to interface

--- Assignment_7_HashTables/src/main/kotlin/AssociativeArray.kt
+++ Assignment_7_HashTables/src/main/kotlin/AssociativeArray.kt
@@ -5,50 +5,38 @@ package org.example
  * @param K the type of the keys
  * @param V the type of the values
  */
-class AssociativeArray<K, V> {
+interface AssociativeArray<K, V> {
     /**
      * Insert the mapping from the key, [k], to the value, [v].
      * If the key already maps to a value, replace the mapping.
      */
-    operator fun set(k: K, v: V) {
-        // your code here
-    }
+    operator fun set(k: K, v: V)
 
     /**
      * @return true if [k] is a key in the associative array
      */
-    operator fun contains(k: K): Boolean {
-        // your code here
-    }
+    operator fun contains(k: K): Boolean
 
     /**
      * @return the value associated with the key [k] or null if it doesn't exist
      */
-    operator fun get(k: K): V? {
-        // your code here
-    }
+    operator fun get(k: K): V?
 
     /**
      * Remove the key, [k], from the associative array
      * @param k the key to remove
      * @return true if the item was successfully removed and false if the element was not found
      */
-    fun remove(k: K): Boolean {
-        // your code here
-    }
+    fun remove(k: K): Boolean
 
 
     /**
      * @return the number of elements stored in the hash table
      */
-    fun size(): Int {
-        // your code here
-    }
+    fun size(): Int
 
     /**
      * @return the full list of key value pairs for the associative array
      */
-    fun keyValuePairs(): List<Pair<K, V>> {
-        // your code here
-    }
+    fun keyValuePairs(): List<Pair<K, V>>
 }
