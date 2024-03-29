20ccc45174b73715c243645871ec2fa29b6ace7c
Ian Lum <ianlum314@gmail.com>
2024-03-28 20:21:58 -0400

    generate numerical representation end char encodes incorrectly

--- Assignment_7_HashTables/src/main/kotlin/LempelZiv.kt
+++ Assignment_7_HashTables/src/main/kotlin/LempelZiv.kt
@@ -3,17 +3,26 @@ package org.example
 fun lempelZiv(str: String) {
     var input = str
     val codebook = HashMap<String, Int>()
+    codebook[""] = 0
+    var out = ""
     while (input.isNotEmpty()) {
-        var subLen = 0
+        var subLen = 1
+        // find substring length for new entry
         while (
             codebook.contains(input.take(subLen)) and
-            (subLen <= input.length) // catch final substring if it matches existing codebook entry
+            // catch final substring if it matches existing codebook entry
+            (subLen <= input.length)
         ) {
             subLen++
         }
         val substr = input.take(subLen)
+        // add to codebook
         codebook[substr] = codebook.size()
+        // remove from input
         input = input.drop(subLen)
+        // add to output
+        out += codebook[substr.dropLast(1)].toString() + substr.last() + " "
     }
     println(codebook.keyValuePairs())
+    println(out)
 }
