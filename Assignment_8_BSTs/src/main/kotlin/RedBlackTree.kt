25b0de852641bdda5b58bd74e2ee11ea54cff4c4
Ian Lum <ianlum314@gmail.com>
2024-04-03 16:17:08 -0400

    impl insert

--- Assignment_8_BSTs/src/main/kotlin/RedBlackTree.kt
+++ Assignment_8_BSTs/src/main/kotlin/RedBlackTree.kt
@@ -7,9 +7,35 @@ class RedBlackTree {
         val value: Int,
         val parent: Node? = null,
         val color: Color = Color.R,
-        val left: Node? = null,
-        val right: Node? = null
+        var left: Node? = null,
+        var right: Node? = null
     )
 
-    val root: Node? = null
+    var root: Node? = null
+
+    /**
+     * Insert a value as a leaf following binary search tree procedure
+     * @param value The value to insert
+     */
+    fun insert(value: Int) {
+        var parent: Node? = null
+        var curr: Node? = root
+
+        // Trace down the tree, going left or right based on inequality
+        while (curr != null) {
+            parent = curr
+            curr = if (value < curr.value)
+                curr.left
+            else
+                curr.right
+        }
+
+        val new = Node(value, parent = parent)
+        if (parent == null)
+            root = new
+        else if (new.value < parent.value)
+            parent.left = new
+        else
+            parent.right = new
+    }
 }
